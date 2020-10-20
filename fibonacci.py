4d802e3c3111af466e65c7ff560dbe4a7418dfe4
Marcos Felipe <65370491+mfalvess@users.noreply.github.com>
2020-10-20 00:09:02 -0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,8 +2,11 @@ import os
 from flask import Flask, jsonify, request
 from math import sqrt
 
+
 app = Flask(__name__)
+
 @app.route('/')
+
 def fibonacci():
     prox = 1
     ant = 0
@@ -16,8 +19,11 @@ def fibonacci():
         ant = y
         x += 1
         resp += str(prox) + ","
+    
     return resp
 
 if __name__ == "__main__":
+    
     port = int(os.environ.get("PORT", 5000))
+    
     app.run(host='0.0.0.0', port=port)
